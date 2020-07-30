69d5e9798d6458885d2af7eb9b2ef9e4ded99ead
Aryan Ganotra <aryanganotra7@gmail.com>
2020-07-30 20:22:24 +0530

    knapsack dp

--- knapsack.py
+++ knapsack.py
@@ -3,6 +3,14 @@ def knapsack(w, v,n, W):
         return float('-inf')
     if n < 0 or W==0:
         return 0
+    include = v[n] + knapsack(w,v,n-1,W-w[n])
+    exclude = knapsack(w,v,n-1,W)
+    return max(include, exclude)
+def knapsackMemoized(w, v,n, W):
+    if W < 0:
+        return float('-inf')
+    if n < 0 or W==0:
+        return 0
     key = (n,W)
     if key not in lookup:
 
@@ -10,10 +18,24 @@ def knapsack(w, v,n, W):
         exclude = knapsack(w,v,n-1,W)
         lookup[key] = max(include, exclude)
     return lookup[key]
+
+def knapsackDP(w,v,n, W):
+    T = [[0 for j in range(W+1)] for i in range(len(v)+1)]
+    for i in range(1, len(v)+1):
+        for j in range(W+1):
+            if (w[i-1] > j):
+                T[i][j] = T[i-1][j]
+            else:
+                T[i][j] = max(T[i-1][j], v[i-1] + T[i-1][j-w[i-1]])
+  
+    return T[len(v)][W]
+
+
+
 lookup = {}
 if __name__=='__main__':
     v = [20, 5, 10, 40, 15, 25]
     w = [1,2,3,8,7,4]
     W = 10
     
-    print(knapsack(w,v,len(w)-1,W))
+    print(knapsackDP(w,v,len(w)-1,W))
