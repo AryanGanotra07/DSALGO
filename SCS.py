aff3936370955001d65cbd5a3e4c334516b4b4af
Aryan Ganotra <aryanganotra7@gmail.com>
2020-08-01 23:12:32 +0530

    print scs

--- SCS.py
+++ SCS.py
@@ -14,7 +14,36 @@ def lcsRecursiveDP(x,y, m, n):
                 T[i][j] = 1 + T[i-1][j-1]
             else:
                 T[i][j] = max(T[i-1][j], T[i][j-1])
-    return T[m][n]
+    return T
+
+def printScs(x,y,m, n,t):
+    i, j = m, n
+    res = []
+    while (i >  0 and j > 0):
+        if x[i-1] == y[j-1]:
+            res.append(x[i-1])
+            i -= 1
+            j -=1
+        else:
+            if (t[i-1][j] > t[i][j-1]):
+                
+                res.append(x[i-1])
+                i -=1
+            else:
+                res.append(y[j-1])
+             
+                j-=1
+    while (i > 0):
+        res.append(x[i-1])
+        i-=1
+    while (j > 0):
+        res.append(y[j-1])
+        j-=1
+
+    res.reverse()
+    return res
+
+
 
 
 if __name__ == '__main__':
@@ -22,4 +51,7 @@ if __name__ == '__main__':
     y = "abedgf"
     m = len(x)
     n = len(y)
-    print(m+n-lcsRecursiveDP(x,y,m,n)) #scs
+    t = lcsRecursiveDP(x,y,m,n)
+    print(t)
+    print(printScs(x,y,m,n,t))
+    # print(m+n-lcsRecursiveDP(x,y,m,n)) #scs
