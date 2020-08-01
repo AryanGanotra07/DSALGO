52ee46247fb12303a25807e42e75afbc435b0029
Aryan Ganotra <aryanganotra7@gmail.com>
2020-08-01 09:12:15 +0530

    memoized LCS

--- LCS.py
+++ LCS.py
@@ -5,6 +5,19 @@ def LCSRecursive(s1, s2, n, m):
         return 1 + LCSRecursive(s1, s2, n-1, m-1)
     else:
         return max(LCSRecursive(s1, s2, n-1, m), LCSRecursive(s1, s2, n, m-1))
+
+
+def LCSMemoized(s1, s2, n, m):
+    if n==0 or m==0:
+        return 0
+    key = (n, m)
+    if key not in lookup:
+        if s1[n-1] == s2[m-1]:
+            lookup[key] = 1 + LCSMemoized(s1, s2, n-1, m-1)
+        else:
+            lookup[key] = max(LCSMemoized(s1, s2, n-1, m), LCSMemoized(s1, s2, n, m-1))
+    return lookup[key]
+lookup = {}
     
 
 if __name__ == '__main__':
@@ -12,4 +25,4 @@ if __name__ == '__main__':
     s2 = "abedfhr"
     n = len(s1)
     m = len(s2)
-    print(LCSRecursive(s1, s2, n, m))
+    print(LCSMemoized(s1, s2, n, m))
