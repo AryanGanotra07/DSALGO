03adb04e221efe4906714d1fa17448c092b67cdc
Aryan Ganotra <aryanganotra7@gmail.com>
2020-08-01 11:19:02 +0530

    lcs dp bottom up

--- LCS.py
+++ LCS.py
@@ -18,11 +18,21 @@ def LCSMemoized(s1, s2, n, m):
             lookup[key] = max(LCSMemoized(s1, s2, n-1, m), LCSMemoized(s1, s2, n, m-1))
     return lookup[key]
 lookup = {}
-    
+
+def LCSDP(s1, s2, n, m):
+    T = [[0 for j in range(m+1)] for i in range(n+1)]
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            if (s1[i-1] == s2[j-1]):
+                T[i][j] = 1 + T[i-1][j-1]
+            else:
+                T [i][j] = max(T[i-1][j], T[i][j-1])
+    return T[n][m]
+     
 
 if __name__ == '__main__':
     s1 = "abcdgh"
     s2 = "abedfhr"
     n = len(s1)
     m = len(s2)
-    print(LCSMemoized(s1, s2, n, m))
+    print(LCSDP(s1, s2, n, m))
