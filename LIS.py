d516bbd249c353067ab5c85e18c508a928046914
Aryan Ganotra <aryanganotra7@gmail.com>
2020-08-02 20:34:29 +0530

    lis dp

--- LIS.py
+++ LIS.py
@@ -19,10 +19,22 @@ def LISMemoized(a, s, n, prev):
         lookup[key] = max(incl, excl)
     return lookup[key]
 
+def LISDP(a):
+    n = len(a)
+    t = [0] * n
+    t[0] = 1
+    for i in range(1,n):
+        for j in range(i):
+            if a[i] > a[j] and t[i] < t[j]:
+                t[i] = t[j]
+        t[i]+=1
+    return max(t)
+
+
 lookup = {}
 if __name__ == '__main__':
  
     A = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
  
-    print("Length of LIS is", LISMemoized(A, 0, len(A), float('-inf')))
+    print("Length of LIS is", LISDP(A))
     
