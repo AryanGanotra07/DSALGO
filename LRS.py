092b968be7251206fe12328c50f5120896f2bdda
Aryan Ganotra <aryanganotra7@gmail.com>
2020-08-02 19:27:40 +0530

    print lrs

--- LRS.py
+++ LRS.py
@@ -6,8 +6,27 @@ def LCSDP(x,y,m,n):
                 T[i][j] = 1 + T[i-1][j-1]
             else:
                 T[i][j] = max(T[i-1][j], T[i][j-1])
-    return T[m][n]
+    return T
+
+def printLRS(x,m,t):
+    i, j = m, m
+    res = ""
+    while (i > 0 and j > 0):
+        if (x[i-1] == x[j-1] and i != j):
+            res= x[i-1]+res
+            i -=1 
+            j-=1
+        else:
+            if t[i-1][j] > t[i][j-1]:
+                i-=1
+            else:
+                j-=1
+    return res
+
+
+
 
 if __name__ == '__main__':
     a = "ATACTCGGA"
-    print(LCSDP(a,a,len(a), len(a)))
+    t = LCSDP(a,a,len(a), len(a))
+    print(printLRS(a,len(a),t))
