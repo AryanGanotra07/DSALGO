529c92ef61e09e06d484e773bcfb3c0d1be2cb70
Aryan Ganotra <aryanganotra7@gmail.com>
2020-08-03 16:56:46 +0530

    edit distance dp

--- editDistance.py
+++ editDistance.py
@@ -7,9 +7,25 @@ def editDistanceecursive(x,m,y,n):
         return editDistanceecursive(x,m-1, y, n-1)
     return min(editDistanceecursive(x,m-1, y, n) + 1, editDistanceecursive(x,m-1,y, n-1) + 1, editDistanceecursive(x, m, y, n-1) + 1)
 
+def editDistanceDp(x, m, y, n):
+    t = [[0 for j in range(n+1)] for i in range(m+1)]
+    for i in range(1, m+1):
+        t[i][0] = 1
+    for j in range(1, n+1):
+        t[0][j] = 1
+    # print(t)
+    for i in range(1, m+1):
+        for j in range(1,n+1):
+            if x[i-1] == y[j-1]:
+                t[i][j] = t[i-1][j-1]
+            else:
+                t[i][j] = min(t[i-1][j-1] +1 , t[i-1][j] + 1, t[i][j-1] + 1)
+    # print(t)
+    return t[m][n]
+
 if __name__ == '__main__':
  
     X = "kitten"
     Y = "sitting"
  
-    print("The Levenshtein Distance is", editDistanceecursive(X, len(X), Y, len(Y)))
+    print("The Levenshtein Distance is", editDistanceDp(X, len(X), Y, len(Y)))
