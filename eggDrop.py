e007c640f9165c0ccf9e71199f4c3181d52dc62b
Aryan Ganotra <aryanganotra7@gmail.com>
2020-08-05 00:10:38 +0530

    memoized egg dropping

--- eggDrop.py
+++ eggDrop.py
@@ -3,11 +3,23 @@ def eggDrop(e,f):
         return f
     if (e == 0):
         return f
+    key = (e,f)
+    if key in lookup:
+        return lookup[key]
     ans = float('inf')
     for k in range(1, f+1):
-        cnt = 1 + max(eggDrop(e-1, k-1), eggDrop(e, f-k)) 
+        kb = (e-1, k-1)
+        if kb not in lookup:
+            lookup [kb] = eggDrop(e-1, k-1)
+        kt = (e, f-k)
+        if kt not in lookup:
+            lookup[kt] = eggDrop(e, f-k)
+        cnt = 1 + max(lookup[kb], lookup[kt]) 
         ans = min(ans, cnt)
+    lookup[key] = ans
     return ans
+
+lookup = {}
 
 if __name__ == "__main__":
     e = 2
