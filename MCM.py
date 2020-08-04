d333810233dfa9a9a26e2c64154dd06662f13785
Aryan Ganotra <aryanganotra7@gmail.com>
2020-08-04 11:26:40 +0530

    mcm recursive

---
+++ MCM.py
@@ -0,0 +1,25 @@
+def MCMRecursive(dims, i, j):
+    if i+ 1 >=j :
+        return 0
+    mn = float('inf')
+    for k in range(i + 1, j):
+ 
+        # recur for M[i+1]..M[k] to get an i x k matrix
+        cost = MCMRecursive(dims, i, k)
+ 
+        # recur for M[k+1]..M[j] to get a k x j matrix
+        cost += MCMRecursive(dims, k, j)
+ 
+        # cost to multiply two (i x k) and (k x j) matrix
+        cost += dims[i] * dims[k] * dims[j]
+ 
+        if cost < mn:
+            mn = cost
+    return mn
+if __name__ == '__main__':
+ 
+    # Matrix M[i] has dimension dims[i-1] x dims[i] for i = 1..n
+    # input is 10 × 30 matrix, 30 × 5 matrix, 5 × 60 matrix
+    dims = [10, 30, 5, 60]
+ 
+    print("Minimum cost is", MCMRecursive(dims, 0, len(dims) - 1))
