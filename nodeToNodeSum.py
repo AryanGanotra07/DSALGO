ab016f8d737ac81df0753b1081fe4c1415cee3bf
Aryan Ganotra <aryanganotra7@gmail.com>
2020-08-05 09:47:14 +0530

    node to node sum in trees

---
+++ nodeToNodeSum.py
@@ -0,0 +1,21 @@
+class Tree:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+def diameter(root, res):
+    if root == None:
+        return 0
+    l = diameter(root.left, res)
+    r = diameter(root.right, res)
+    temp = max(max(l,r) + root.val, root.val)
+    ans = max(temp, root.val + r + l)
+    res = max(ans, res)
+    return temp
+
+if __name__ == "__main__":
+    root = Tree(1)
+    root.left = Tree(0)
+    root.right = Tree(2)
+    res = 0
+    print(diameter(root, res))
