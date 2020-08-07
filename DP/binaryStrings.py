adad3cad0025c2b676aaaf926d8d0904d7ea9f7a
Aryan Ganotra <aryanganotra7@gmail.com>
2020-08-07 11:23:38 +0530

    dp binary strings

--- DP/binaryStrings.py
+++ DP/binaryStrings.py
@@ -7,7 +7,16 @@ def solve(n, ld):
         return solve(n-1, 1) + solve(n-1, 0)
     return solve(n-1, 0)
 
+def solveDP(n):
+    t=[[0 for x in range(2)]for i in range(n+1)]
+    t[1][0] = 2
+    t[1][1] = 1
+    for i in range(2, n+1):
+        t[i][0] = t[i-1][0]  + t[i-1][1]
+        t[i][1] = t[i-1][0]
+    return t[n][0]
+
 
 if __name__ == "__main__":
     n  = 5
-    print(solve(5, 0))
+    print(solveDP(5))
