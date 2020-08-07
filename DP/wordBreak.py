2afc770b37fe53ee2098a52e785a62c9b96d3818
Aryan Ganotra <aryanganotra7@gmail.com>
2020-08-07 12:48:37 +0530

    Word break memoized

--- DP/wordBreak.py
+++ DP/wordBreak.py
@@ -15,6 +15,35 @@ def solve2(s,d):
             return True
     return False
 
+def wordBreak(dict, str, lookup):
+ 
+    # n stores length of current substring
+    n = len(str)
+ 
+    # return true if we have reached the end of the String
+    if n == 0:
+        return True
+ 
+    # if sub-problem is seen for the first time
+    if lookup[n] == -1:
+ 
+        # mark sub-problem as seen (0 initially assuming String
+        # can't be segmented)
+        lookup[n] = 0
+ 
+        for i in range(1, n + 1):
+            # consider all prefixes of current String
+            prefix = str[:i]
+ 
+            # if prefix is found in dictionary, then recur for suffix
+            if prefix in dict and wordBreak(dict, str[i:], lookup):
+                # return true if the can be segmented
+                lookup[n] = 1
+                return True
+ 
+    # return solution to current sub-problem
+    return lookup[n] == 1
+
 
 
 
@@ -25,7 +54,9 @@ if __name__ == "__main__":
         "break", "b", "r", "e", "a", "k", "br",
         "bre", "brea", "ak", "problem"
     ]
+    
  
     # input String
     str = "Wordbreakproblem"
-    print(solve2(str, dict))
+    lookup = [-1] * (len(str) + 1)
+    print(wordBreak(dict,str, lookup))
